ec42caa0e6b7ed63496d45417b5fc7673886e754
DouglasRBS <Douglasribowski17@gmail.com>
2025-11-21 11:58:57 -0300

    Commit 11

--- Movie.java
+++ Movie.java
@@ -1,23 +1,38 @@
 public class Movie {
 
+    // constantes ainda usadas pelas subclasses de Price
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+
+    // *** passo 3: substituir o priceCode por um Price ***
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
