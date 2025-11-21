fc6c7a2da16776d424d3095e97628ae22c1bc3c0
DouglasRBS <Douglasribowski17@gmail.com>
2025-11-21 11:37:50 -0300

    Commit 8

--- Customer.java
+++ Customer.java
@@ -39,6 +39,30 @@ public class Customer {
         return result;
     }
 
+    // ---------------- NOVA FEATURE ----------------
+    // imprime o comprovante em HTML
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+
+            // show figures for each rental
+            result += each.getMovie().getTitle() + ": " +
+                    String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+
+        // add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+                String.valueOf(getTotalFrequentRenterPoints()) +
+                "</EM> frequent renter points<P>";
+
+        return result;
+    }
+    // ---------------- FIM DA NOVA FEATURE ----------------
+
     // calcula o total das cobranças
     private double getTotalCharge() {
         double result = 0;
