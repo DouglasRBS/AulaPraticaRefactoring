bb7ada443292c5dccd38ab30088111632ecd70ed
DouglasRBS <Douglasribowski17@gmail.com>
2025-11-21 11:31:49 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -41,4 +41,14 @@ public class Rental {
 
         return thisAmount;
     }
+
+    // método extraído do Customer e movido para cá
+    public int getFrequentRenterPoints() {
+        // add frequent renter points
+        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            this.getDaysRented() > 1)
+            return 2;  // bônus
+
+        return 1;      // padrão
+    }
 }
